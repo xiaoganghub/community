7fbcd6e5e15962e5368f012477749c61e6695b6a
MrGang <1070252610@qq.com>
2019-11-10 10:37:18 +0800

    add mybatis

--- src/main/java/com/gang/community/controller/AuthorizeController.java
+++ src/main/java/com/gang/community/controller/AuthorizeController.java
@@ -2,12 +2,17 @@ package com.gang.community.controller;
 
 import com.gang.community.dto.AccessTokenDTO;
 import com.gang.community.dto.GithubUser;
+import com.gang.community.mapper.UserMapper;
+import com.gang.community.model.User;
 import com.gang.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 /**
  *
@@ -25,20 +30,37 @@ public class AuthorizeController {
     private String clientSecret;
 
     @Value("${github.redirect.uri}")
-    private String  redirectUri;
+    private String redirectUri;
+
+    @Autowired(required = false)
+    private UserMapper userMapper;
 
 
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state) {
-        AccessTokenDTO accessTokenDTO =  new AccessTokenDTO();
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state, HttpServletRequest request) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser githubUser = githubProvider.getUser(accessToken);
-        System.out.println(githubUser);
-        return "index";
+        GithubUser githubUseruser = githubProvider.getUser(accessToken);
+        System.out.println(githubUseruser);
+        if (githubUseruser != null) {
+            //登录成功，写cookie session
+            User user = new User();
+            user.setName(githubUseruser.getName());
+            user.setToken(UUID.randomUUID().toString());
+            user.setAccountId(githubUseruser.getId());
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUseruser);
+            return "redirect:/";
+        } else {
+            //登录失败
+            return "redirect:/";
+        }
     }
 }
---
+++ src/main/java/com/gang/community/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.gang.community.mapper;
+
+import com.gang.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ *
+ */
+@Mapper
+public interface UserMapper {
+
+    @Insert("insert into user(name,account_id,token,gmt_create,gmt_modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+}
